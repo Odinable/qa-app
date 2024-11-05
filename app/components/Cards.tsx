fae419b38dd0b8cf4da47dd8ba6672fada6cfc63
Odinable <rohan9433@yahoo.com>
2024-11-06 04:12:28 +0500

    fixed typrscript error

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -5,76 +5,76 @@ import React, { useState } from "react";
 const Cards = () => {
   const questions = [
     {
-      id: 1,
+      id: "1",
       question: "What language is React based on?",
       answer: "JavaScript",
       color: "green",
     },
     {
-      id: 2,
+      id: "2",
       question: "What are the building blocks of React apps?",
       answer: "Components",
       color: "brown",
     },
     {
-      id: 3,
+      id: "3",
       question:
         "What's the name of the syntax we use to describe a UI in React?",
       answer: "JSX",
       color: "blue",
     },
     {
-      id: 4,
+      id: "4",
       question: "How to pass data from parent to child components?",
       answer: "Props",
       color: "yellow",
     },
     {
-      id: 5,
+      id: "5",
       question: "How to give components memory?",
       answer: "useState hook",
       color: "red",
     },
     {
-      id: 6,
+      id: "6",
       question:
         "What do we call an input element that is completely synchronised with state?",
       answer: "Controlled element",
       color: "purple",
     },
     {
-      id: 7,
+      id: "7",
       question: "What is the primary purpose of React?",
       answer: "UI",
       color: "green",
     },
     {
-      id: 8,
+      id: "8",
       question:
         "What hook is used for managing state in a functional component?",
       answer: "useState",
       color: "brown",
     },
     {
-      id: 9,
+      id: "9",
       question: "What is used to pass data between components in React?",
       answer: "Props",
       color: "blue",
     },
     {
-      id: 10,
+      id: "10",
       question: "What is the syntax used to describe UI in React?",
       answer: "JSX",
       color: "yellow",
     },
     {
-      id: 11,
+      id: "11",
       question: "What function is used to render a React component to the DOM?",
       answer: "ReactDOM.render",
       color: "red",
     },
     {
-      id: 12,
+      id: "12",
       question:
         "What React hook is used for side effects in functional components?",
       answer: "useEffect",
